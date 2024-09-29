a27cd9a6d408d02bc3b50d0b148f00a7a8f42f74
Soh Ohara <wildgeece96@gmail.com>
2024-09-29 15:11:35 +0900

    Changed variables implementation

--- infra/private-saas-vpc/subnet_public.tf
+++ infra/private-saas-vpc/subnet_public.tf
@@ -5,9 +5,9 @@ resource "aws_subnet" "public" {
   cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
-  tags = {
+  tags = merge(local.tags, {
     Name = "${local.tags.Project}: Public Subnet ${count.index + 1}"
-  }
+  })
 }
 
 # Internet Gateway
@@ -27,9 +27,9 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.main.id
   }
 
-  tags = {
-    Name = "${local.tags.Project}: Public Route Table"
-  }
+  tags = merge(local.tags, {
+    Name = "private-saas-tg"
+  })
 }
 
 # Route Table Associations
@@ -44,9 +44,9 @@ resource "aws_eip" "nat" {
   count  = var.enable_multi_nat ? var.az_count : 1
   domain = "vpc"
 
-  tags = {
+  tags = merge(local.tags, {
     Name = "${local.tags.Project}: NAT Gateway EIP ${count.index + 1}"
-  }
+  })
 
   depends_on = [aws_internet_gateway.main]
 }
@@ -57,7 +57,7 @@ resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
-  tags = {
+  tags = merge(local.tags, {
     Name = "${local.tags.Project}: NAT Gateway ${count.index + 1}"
-  }
+  })
 }
